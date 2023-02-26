42431b8b7b9bf364620e39b006d66c65a20c39c8
ThePianist <cafarimo@outlook.com>
2023-02-26 10:13:57 -0300

    Added Melocoton

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,12 @@ function App() {
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="absolute inset-0 -z-20 bg-[url(/src/assets/beams.jpg)] bg-center" />
       <div className="absolute inset-0 -z-10 bg-[url(/src/assets/grid.svg)] bg-center" />
+      <div
+        className="absolute right-10 bottom-10 cursor-grabbing rounded-full bg-white bg-opacity-80 py-4 px-5 text-2xl text-violet-400 drop-shadow-md"
+        alt="love u melocoton"
+      >
+        Guau! 🐶
+      </div>
       {img ? (
         <Editor img={img} />
       ) : (
